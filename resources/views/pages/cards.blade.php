8292bb7a8c411ef2b369583ff28af30e2d15ecca
sirio95 <sirio.iasevoli@gmail.com>
2023-01-30 20:35:15 +0100

    jumbotron + cards

---
+++ resources/views/pages/cards.blade.php
@@ -0,0 +1,5 @@
+<section id="card">
+    <div class="container">
+        @include('components/card_data');
+    </div>
+</section>
---
+++ resources/views/pages/header.blade.php
@@ -0,0 +1,40 @@
+<section id="header">
+    <div class="container">
+        <div class="row m-0 thin_blue">
+            <div class="col-12 d-flex justify-content-end">
+                <a href="#">DC POWERED</a>
+                <a href="#">ADDITIONAL DC SITES &#9660;</a>
+            </div>
+        </div>
+        <div class="row m-0 nav_bar">
+            <div class="col-12 d-flex justify-content-around">
+                <div class="h-70 w-70 logo_img">
+                    <img src="{{ Vite::asset('/resources/imgs/favicon.ico') }}" alt="dc icon">
+                </div>
+                <ul class="d-flex justify-content-around align-items-center gap-3 nav_bar_links">
+                    <li><a href='#' class='text-uppercase'>characters</a></li>
+                    <li class='selected'><a href='#' class='text-uppercase'>comics</a></li>
+                    <li><a href='#' class='text-uppercase'>movies</a></li>
+                    <li><a href='#' class='text-uppercase'>tv</a></li>
+                    <li><a href='#' class='text-uppercase'>games</a></li>
+                    <li><a href='#' class='text-uppercase'>collectibles</a></li>
+                    <li><a href='#' class='text-uppercase'>videos</a></li>
+                    <li><a href='#' class='text-uppercase'>fans</a></li>
+                    <li><a href='#' class='text-uppercase'>news</a></li>
+                    <li><a href='#' class='text-uppercase'>shop</a> <span><a href="#">&#9660;</a></span></li>
+                </ul>
+                <div class="input-group search_form">
+                    <div class="form-outline">
+                      <input id="search-focus" type="search" id="form1" class="form-control search_box" placeholder="Search"/>
+                    </div>
+                    <button type="button" class="btn search_btn">
+                        &#x1F50D;
+                    </button>
+                </div>
+            </div>    
+        </div>
+        <div class="row m-0 hero"></div>
+    </div>
+        
+</section>
+
