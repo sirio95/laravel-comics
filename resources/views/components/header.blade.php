c53bbe72d4b5e329eeb79c4795ea85afb308392a
sirio95 <sirio.iasevoli@gmail.com>
2023-01-30 16:43:21 +0100

    aggiunte immagini e strutturazione cartelle

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.general');
+@section('title')
+    <title>header</title>
+@endsection
+@section('content')
+    
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
+});
+
+Route::get('/comic_one', function () {
+    return view('comic_one');
 });
